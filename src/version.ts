71e147315ad5c99ca8142ad7a036e2262f5c6475
Done-0 <fenderisfine@outlook.com>
2026-03-18 23:13:13 +0800

    chore: update version to 2.2.2 in package.json and version.ts

--- src/version.ts
+++ src/version.ts
@@ -2,4 +2,4 @@
  * Application version - single source of truth
  * This value is automatically synced from package.json during build
  */
-export const VERSION = '2.2.1';
+export const VERSION = '2.2.2';
